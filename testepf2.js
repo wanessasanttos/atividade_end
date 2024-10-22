c6002487264f5465e3394b79f6b65a2b4152e82b
Wanessa <159792112+wanessasanttos@users.noreply.github.com>
2024-10-21 22:24:39 -0300

    versão teste ok

--- testepf2.js
+++ testepf2.js
@@ -1,51 +1,301 @@
-const CursoPeriodo = (x, y) => {
-    if (x == "Sistemas de Informação" && y == 1) return { PrimeiroPeriodo: [
-        { Disciplina: 'Introdução à Administração', ch: 60 },
-        { Disciplina: 'Programação Funcional', ch: 60 },
-        { Disciplina: 'Vetores e Geometria Analítica', ch: 60 },
-        { Disciplina: 'Cálculo A', ch: 60 },
-        { Disciplina: 'Seminários em Computação', ch: 30 }
-    ]};
-    if (x == "Sistemas de Informação" && y == 2) return { SegundoPeriodo: [
-        { Disciplina: 'Cálculo B', ch: 60 },
-        { Disciplina: 'Fundamentos Elementares da Matemática', ch: 60 },
-        { Disciplina: 'Programação Imperativa', ch: 60 },
-        { Disciplina: 'OSM', ch: 60 },
-        { Disciplina: 'Informática, Ética e Sociedade', ch: 60 }
-    ]};
-    return undefined;
+const sistemasDeInformacao = [
+    [
+        'Introdução à Administração - ch: 60',
+        'Programação Funcional - ch: 60',
+        'Seminários em Computação - ch: 30',
+        'Vetores e geometria analítica - ch: 60',
+        'Cálculo A - ch: 60'
+    ],
+
+    [
+        'Organização, Métodos e Sistemas Administrativos - ch: 60',
+        'Programação Imperativa - ch: 60',
+        'Informática, Ética e Sociedade - ch: 60',
+        'Fundamentos Elementares da Matemática - ch: 60',
+        'Cálculo B - ch: 60'
+    ],
+
+    [
+        'Programação Orientada A Objeto - ch: 60',
+        'Estrutura de Dados - ch: 60',
+        'Lógica para programação - ch: 60',
+        'Arquitetura de Computadores - ch: 60',
+        'Estatistica Aplicada - ch: 60'
+    ],
+
+    [
+        'Sociologia da Organizações - ch: 60',
+        'Grafos e Algoritmos Computacionais - ch: 60',
+        'Projeto e Análise de Algoritmos - ch: 60',
+        'Sistemas Operacionais - ch: 60',
+        'Métodos e Técnicas de Pesquisa para Computação - ch: 30'
+    ],
+
+    [
+        'Linguagens Formais e Computabilidade - ch: 60',
+        'Engenharia de Software I - ch: 60',
+        'Teoria Geral Dos Sistemas - ch: 60',
+        'Banco De Dados I - ch: 60',
+        'Redes De Computadores - ch: 60'
+    ],
+
+    [
+        'Inteligência Artificial - ch: 60',
+        'Egenharia De Software II - ch: 60',
+        'Sistemas De Apoio à Decisão - ch: 60',
+        'Laboratório De Redes De Computadores - ch: 30',
+        'Sistemas Distribuidos - ch: 60'
+    ],
+
+    [
+        'Empreendedorismo E Gestão De Pequenos Negócios - ch: 60',
+        'Teste De Software - ch: 60',
+        'Gestão De Sistemas de Informação - ch: 60'
+    ],
+
+    [
+        'Qualidade De Software - ch: 60',
+        'Evolução De Software - ch: 60',
+        'Gerência De Projetos - ch: 60'
+    ],
+
+    [
+        'Prática Orientada Em Computação I - ch: 180',
+        'Trabalho De Conclusão De Curso I - ch: 60'
+    ],
+
+    [
+        'Estágio Supervisionado Em Computação - ch: 210',
+        'Prática Orientada Em Computação II - ch: 120',
+        'Trabalho De Conclusão De Curso II - ch: 120'
+    ]
+]
+
+const cienciaDaComputacao = [
+    [
+        'Programação Funcional - ch: 60',
+        'Seminários Em Computação - ch: 30',
+        'Fundamento Elementares Da Matemática - ch: 30',
+        'Vetores E Geometria Analítica - ch: 60',
+        'Cálculo A - ch: 60'
+    ],
+
+    [
+        'Programação Imperativa - ch: 60',
+        'Lógica Para Computação - ch: 60',
+        'Física 1 - ch: 60',
+        'Laboratório De Física 1 - ch: 30',
+        'Álgebra Linear I - ch: 60',
+        'Cálculo B - ch: 60'
+    ],
+
+    [
+        'Programação Orientada A Objeto - ch: 60',
+        'Estrutura de Dados - ch: 60',
+        'Linguagens Formais E Computabilidade - ch: 60',
+        'Fundamentos De Sistemas Digitais  - ch: 60',
+        'Prática Em Sistemas Digitais - ch: 30',
+        'Cálculo C - ch: 60',
+    ],
+
+    [
+        'Grafos E Algoritmos Computacionais - ch: 60',
+        'Projeto E Análise De Algoritmos - ch: 60',
+        'Arquitetura De Computadores - ch: 60',
+        'Fundamentos De Sistemas Embarcados - ch: 30',
+        'Métodos E Técnicas De Pesquisa Para Computores - ch: 30',
+        'Estatistica Aplicada - ch: 60',
+    ],
+
+    [
+        'Programação Paralela E Concorrente - ch: 60',
+        'Engenharia De Software I - ch: 60',
+        'Banco De Dados I - ch: 60',
+        'Redes De Computadores - ch: 60',
+        'Sistemas Operacinais - ch: 60',
+        'Informática Ética E Sociedade - ch: 60'
+    ],
+
+    [
+        'Inteligência Artificial - ch: 60',
+        'Processamento De Imagens - ch: 60',
+        'Engenharia De Software II - ch: 60',
+        'Interface Humano-Computador - ch: 60',
+        'Laboratório De Redes De Computadores  - ch: 30',
+        'Sistemas Distribuídos - ch: 60'
+    ],
+
+    [
+        'Compiladores - ch: 60',
+        'Prática Orientada Em Computação I - ch: 180'
+    ],
+
+    [
+        'Prática Orientada Em Computação II - ch: 120',
+        'Trabalho De Conclusão De Curso I - ch: 60'
+    ],
+
+    [
+        'Atividades Complementares - ch: 120',
+        'Estágio Supervisionado Em Computação - ch: 210',
+        'Trabalho De Conclusão De Curso II - ch: 120'
+    ]
+]
+
+const engenhariaDaComputacao = [
+    [
+        'Programação Funcional - ch: 60',
+        'Informática, Ética E Sociedade - ch: 60',
+        'Seminários Em Computação - ch: 30',
+        'Fundamentos Elementares Da Matemática - ch: 60',
+        'Vetores E Geometria Analítica - ch: 60',
+        'Cálculo A - ch: 60'
+    ],
+
+    [
+        'Programação Imperativa - ch: 60',
+        'Lógica Para Computação - ch: 60',
+        'Física 1 - ch: 60',
+        'Laboratório De Física - ch: 30',
+        'Álgebra Linear I - ch: 60',
+        'Calculo B  - ch: 60'
+    ],
+
+    [
+        'Programação Orientada A Objeto - ch: 60',
+        'Estrutura De Dados - ch: 60',
+        'Fundamentos De Sistemas Digitais - ch: 60',
+        'Prática Em Sistemas Digitais - ch: 30',
+        'Métodos E Técnicas De Pesqusia Para Computação - ch: 30',
+        'Física 3 - ch: 60',
+        'Cálculo C - ch: 60'
+    ],
+
+    [
+        'Linguagens Formais E Computabilidade - ch: 60',
+        'Projeto E Análise De Algoritmos - ch: 60',
+        'Arquitetura De Computadores - ch: 60',
+        'Fundamentos De Sistemas Embarcados - ch: 30',
+        'Introdução À Instrumentação - ch: 30',
+        'Estatística Aplicada - ch: 60',
+        'Cálculo D - ch: 60',
+        'Equações Diferenciais I - ch: 60'
+    ],
+
+    [
+        'Grafos E Algoritmos Computacionais - ch: 60',
+        'Engenharia De Software I - ch: 60',
+        'Redes De Computadores - ch: 60',
+        'Sistemas Operacionais - ch: 60',
+        'Análise De Sistemas Lineares - ch: 60',
+        'Circuitos Elétricos I - ch: 90'
+    ],
+    
+    [
+         'Programação Paralela E Concorrente - ch: 60',
+         'Inteligência Artificial - ch: 60',
+         'Engenharia De Software II - ch: 60',
+         'Banco De Dados I - ch: 60',
+         'Laboratório De Redes De Computadores - ch: 30',
+         'Sistemas Distribuídos - ch: 60',
+         'Eletrônica I - ch: 90'
+    ],
+
+    [
+         'Interface Hardware/Software - ch: 60',
+         'Controle - ch: 90',
+         'Cálculo Numérico I - ch: 60',
+    ],
+
+    [
+         'Compiladores - ch: 60',
+         'Prática Orientada Em Computação - ch: 180',
+         'Processamento Digital De Sinais - ch: 60'
+    ],
+
+    [
+         'Prática Orientada Em Computação II - ch: 120',
+         'Prática De Conclusão De Curso - ch: 60'
+    ],
+    
+    [
+         'Atividades Complementares - ch: 120',
+         'Estágio Supervisionado Em Computação - ch: 210',
+         'Trabalho De Conclusão de Curso II - ch: 120'
+    ]
+]
+
+
+const disciplinas = {
+    sistemasDeInformacao,
+    cienciaDaComputacao,
+    engenhariaDaComputacao
 }
 
-const Nome = () => {
-    const ExecutarNome = document.getElementById("Nome").value;
-    document.getElementById("output").innerHTML = `Olá, ${ExecutarNome}.`;
+const mostrarBoasVindas = () => {
+    // Obter o valor do input de nome
+    const nome = document.getElementById("nome").value
+    
+    // Exibir mensagem de boas-vindas na tela
+    const mensagem = `Bem-vindo(a),  ${nome} !`
+    document.getElementById("mensagem").innerText = mensagem
+}
+ 
+const confirmarCurso = () => {
+    // Recebe o valor do curso selecionado no input do HTML
+    const curso = document.getElementById('curso').value
+
+    // Seleciona a saída da mensagem no html
+    const confirmacaoDiv = document.getElementById('confirmacaoCurso')
+
+    // Verifica o curso e exibe a mensagem 
+    if (curso === 'sistemasDeInformacao') {
+        confirmacaoDiv.innerHTML = '<p>Você está cursando Sistemas de Informação!</p>'
+    } else if (curso === 'cienciaDaComputacao') {
+        confirmacaoDiv.innerHTML = '<p>Você está cursando Ciência da Computação!</p>'
+    } else if (curso === 'engenhariaDaComputacao') {
+        confirmacaoDiv.innerHTML = '<p>Você está cursando Engenharia da Computação!</p>'
+    } else {
+        // Caso não tenha sido selecionado um curso válido
+        confirmacaoDiv.innerHTML = '<p>Por favor, selecione um curso válido.</p>'
+    }
 }
 
-const ExecutarCurso = () => {
-    const curso = document.getElementById("SistemasDeInformação").value;
-    const periodo = document.getElementById("Período").value;
-    const resultado = CursoPeriodo(curso, periodo);
-    document.getElementById("output").innerHTML = JSON.stringify(resultado);
-}
 
-const Limpar = () => {
-    document.getElementById("output").innerHTML = "";
-}
+const mostrarDisciplinas = () => {
+    //Recebe o curso declarado na entrada do html 
+    const curso = document.getElementById('curso').value
+    //Recebe o periodo declarado no html, transforma de string para int, e subtrai 1 para ajustar com os indices dos arrays 
+    const periodo = parseInt(document.getElementById('periodo').value) - 1
 
-function selecionarCurso(cursoSelecionado) {
-    var cursos = document.querySelectorAll('.curso');
-    cursos.forEach(function (curso) {
-        curso.style.display = 'none';
-    });
-    document.getElementById(cursoSelecionado).style.display = 'block';
-    document.getElementById('curso-nav').style.display = 'none';
-}
+    //Seleciona o local do html onde o resultado será exibido 
+    const resultadoDiv = document.getElementById('resultado')
 
-function selecionarPeriodo(periodoSelecionado) {
-    var periodos = document.querySelectorAll('.periodo');
-    periodos.forEach(function (periodo) {
-        periodo.style.display = 'none';
-    });
-    document.getElementById(periodoSelecionado).style.display = 'block';
-    document.getElementById('periodo-nav').style.display = 'none';
-}
+    //Serve para verificar se o usuario selecionol um curso e um periodo valido
+    // Se for invalido, o programa exibira na tela o a string declarada no else 
+    if (curso && !isNaN(periodo)) {
+        
+        // Essa função acessa a lista de disciplinas buscando atraves do curso e do periodo declarado na entrada e guarda esse resultado na const lista de disciplinas
+        const listaDisciplinas = disciplinas[curso][periodo]
+
+            // Esse if testa se o numero de periodos declarados coincide com o numero de periodos que o curso possui
+            // Caso não possua, ele irá exibir a string definida no else 
+        if (listaDisciplinas) {
+
+            // Esse trecho define o conteudo da contante resultado.Div, que é o que será exibido na interface html 
+            resultadoDiv.innerHTML = `<h2>As disciplinas do ${periodo + 1}º Período são as seguintes:</h2><ul>` + 
+                                // A partir do + mapeia a lista das disciplinas. Ela passa elemento por elemento e mapeia cada uma para o elemento '<li>', que significa um elemento de uma lista 
+                                // Esse elemento mapeado vai ser guardado dentro do elemento '<ul>' que é uma lista de elementos sem uma numeração definida para cada elemento, mas com marcadores
+                                // O elemento .joins('') combina os elementos definidos em <li> para a lista <ul>
+                                listaDisciplinas.map(disciplina => `<li>${disciplina}</li>`).join('') +
+                                '</ul>'
+        } else {
+            // Mensagem de erro se o numero de periodos não coincidir com o número de periodos que o curso possui
+            resultadoDiv.innerHTML = `<p>O curso selecionado não possui ${periodo + 1} períodos.</p>`
+        }
+    } else {
+        // Mensagem de erro mostrada quando um dos elementos não é selecionado
+        resultadoDiv.innerHTML = '<p>Por favor, selecione um curso e um período.</p>'
+    }
+} 
+
